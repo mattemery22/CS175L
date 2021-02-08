42406540c778229f4391d6ac0d56366f290cd97b
mattemery22 <mattemery22@localhost>
2021-02-08 12:40:55 -0500

    House Painting - Version 3

--- housePainting/src/housePainting/housePaint.java
+++ housePainting/src/housePainting/housePaint.java
@@ -27,34 +27,34 @@ public class housePaint {
 		
 		
 
-		System.out.println("Enter length of house (horizontal):");
+		System.out.print("Enter length of house (horizontal): ");
 		houseLength = in.nextDouble();
 		
-		System.out.println("Enter width of house(vertical not including peak):");
+		System.out.print("Enter width of house(vertical not including peak): ");
 		houseWidth = in.nextDouble();
 
-		System.out.println("Enter height of house (base of house to peak):");
+		System.out.print("Enter height of house (base of house to peak): ");
 		houseHeight = in.nextDouble();
 		
-		System.out.println("Enter the number of windows on the house:");
+		System.out.print("Enter the number of windows on the house: ");
 		numberOfWindows = in.nextInt();
 		
-		System.out.println("Enter length of one window (horizontal):");
+		System.out.print("Enter length of one window (horizontal): ");
 		lengthOfWindows = in.nextDouble();
 		
-		System.out.println("Enter width of one window (vertical):");
+		System.out.print("Enter width of one window (vertical): ");
 		widthOfWindows = in.nextDouble();
 		
-		System.out.println("Enter the number of doors on the house:");
+		System.out.print("Enter the number of doors on the house: ");
 		numberOfDoors = in.nextInt();
 		
-		System.out.println("Enter length of one door on the house (horizontal):");
+		System.out.print("Enter length of one door on the house (horizontal): ");
 		lengthOfDoors = in.nextDouble();
 		
-		System.out.println("Enter width of one door on the house (vertical):");
+		System.out.print("Enter width of one door on the house (vertical): ");
 		widthOfDoors = in.nextDouble();
 		
-		System.out.println("Enter the cost per square foot:");
+		System.out.print("Enter the cost per square foot:");
 		costPerSqFt = in.nextDouble();
 		
 
@@ -67,6 +67,7 @@ public class housePaint {
 		double finalTotalSqFt = preTotalSqFt - windowsSqFt - doorsSqFt;
 		double totalCost = finalTotalSqFt*costPerSqFt;
 		
+		System.out.println();
 		System.out.println("The total square footage is "+finalTotalSqFt);
 		System.out.println("Your cost estimate is $"+df2.format(totalCost));
 		in.close();
