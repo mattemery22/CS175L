8b51a785fba91c341a24084c8d9bdf903add75b4
mattemery22 <mattemery22@192.168.1.139>
2021-02-18 13:12:40 -0500

    Mail Merge V2 with Replace

--- mailMerge/src/mailMerge/mailMerge.java
+++ mailMerge/src/mailMerge/mailMerge.java
@@ -1,4 +1,4 @@
-package mailMerge;
+ package mailMerge;
 import java.util.Scanner;
 
 public class mailMerge {
@@ -6,42 +6,53 @@ public class mailMerge {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		//Creating scanner
 		Scanner in = new Scanner (System.in);
 		
+		String replaceText1 = "Dear <firstName> <lastName>,";
+		String replaceText2 = "Please confirm that your address is <houseNumber> <street>.";
+		String replaceText3 = "Your answer is: <yORn> Goodbye!";
+		
 		//getting info from user
 		String promptText1="Please enter your first name: ";
+		String promptText2="Please enter your last name: ";
+		String promptText3="Please enter your house number: ";
+		String promptText4="Please enter your street: ";
+		String promptText5="Is this address correct? ";
+		
+		
 		System.out.print(promptText1);
 		String firstName = in.nextLine();
 		
-		String promptText2="Please enter your last name: ";
+		
 		System.out.print(promptText2);
 		String lastName = in.nextLine();
-		
-		String promptText3="Please enter your house number: ";
+
 		System.out.print(promptText3);
 		String houseNumber = in.nextLine();
-		
-		String promptText4="Please enter your street: ";
+
 		System.out.print(promptText4);
 		String street = in.nextLine();
 		
-		
-		
-		//entering user info into text
-		String replaceText1 = "Dear "+firstName+" "+lastName+",";
-		String replaceText2 = "Please confirm that your address is " + houseNumber + " "+street+".";
+	
 		
 		//outputting text with user info
 		System.out.println();
+		replaceText1=replaceText1.replace("<firstName>", firstName);
+		replaceText1=replaceText1.replace("<lastName>", lastName);
 		System.out.println(replaceText1);
+		
+		replaceText2=replaceText2.replace("<houseNumber>", houseNumber);
+		replaceText2=replaceText2.replace("<street>", street);
 		System.out.println(replaceText2);
 		
 		//confirming info with user
-		String promptText5="Is this address correct? ";
 		System.out.print(promptText5);
 		String yesNo = in.nextLine();
-		String replaceText3 = "Your answer is: " + yesNo + " Goodbye!";
+		
+		
+		replaceText3=replaceText3.replace("<yORn>", yesNo);
 		
 		//ending program
 		System.out.println(replaceText3);
